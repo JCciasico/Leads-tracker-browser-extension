3ad2f8fda204b80354ab9cd7500422949551dca8
JCCiasico <carlociasico01@gmail.com>
2022-09-21 14:33:24 +0800

    chore: added save tab

--- js/main.js
+++ js/main.js
@@ -5,33 +5,51 @@ let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
+const delBtn = document.getElementById('del-btn');
+const tabBtn = document.getElementById('tab-btn');
 
-let leadsFromLocal = (JSON.parse(localStorage.getItem("myLeads")));
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+const leadsFromLocal = (JSON.parse(localStorage.getItem("myLeads")));
 
 if(leadsFromLocal){
     myLeads = leadsFromLocal;
-    renderLeads();
+    render(myLeads);
 }
-
-inputBtn.addEventListener("click",function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
-});
-
-
-function renderLeads(){
+function render(leads){
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++){
+    for (let i = 0; i < leads.length; i++){
         // const li = document.createElement('li');
         // li.textContent = myLeads[i];
         // ulEl.append(li);
     
         listItems  += `
         <li>
-            <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+            <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
         </li>`;
     }
     ulEl.innerHTML = listItems;
 }
+delBtn.addEventListener("dblclick",function(){
+    console.log('double clicked');
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
