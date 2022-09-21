60c91f70683e07383c354707777ac0ae2be9f719
JCCiasico <carlociasico01@gmail.com>
2022-09-21 13:48:29 +0800

    chore: added local storage

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 
 let myLeads = [];
 
+
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 
+let leadsFromLocal = (JSON.parse(localStorage.getItem("myLeads")));
+
+if(leadsFromLocal){
+    myLeads = leadsFromLocal;
+    renderLeads();
+}
+
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
-    renderLeads();
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
 });
 
 
-renderLeads=()=>{
+function renderLeads(){
     let listItems = "";
     for (let i = 0; i < myLeads.length; i++){
         // const li = document.createElement('li');
